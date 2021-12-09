2cb930276be1d91c315f7d432d57b7567691d11f
mudziok <mudziok@gmail.com>
2021-12-09 01:04:29 +0100

    Solve Day 8 with a new idea

--- 2021/day8.ts
+++ 2021/day8.ts
@@ -1,55 +1,54 @@
 import { read } from "../utils"
 
-const diff = (a: string[], b: string[]): string[] => a.filter(x => !b.includes(x));
+const diff = (a: string, b: string): string[] => a.split("").filter(x => !b.split("").includes(x));
 
-const digit_codes = ["abcefg", "cf", "acdeg", "acdfg", "bcdf", "abdfg", "abdefg", "acf", "abcdefg", "abcdfg"];
-const codeToDigit = (code: string): number => digit_codes.indexOf(code);
-
-const getDecoder = (unique: string[][]): Map<string, string> => {
-    const symbols = [...new Set(unique.flat())];
-    const occurrences = symbols.map(letter => {
-        const counts = unique.flat().filter(l => l == letter).length;
-        return {letter, counts};
-    });
-    const decoder = symbols.reduce((acc, cur) => acc.set(cur, "?"), new Map<string, string>());
-    decoder.set(occurrences.find(x => x.counts === 6)!.letter, "b");
-    decoder.set(occurrences.find(x => x.counts === 4)!.letter, "e");
-    decoder.set(occurrences.find(x => x.counts === 9)!.letter, "f");
-
+const getDecoder = (unique: string[]) => {
+    const decoder = unique.reduce((acc, cur) => acc.set(cur, -1), new Map<string, number>());
+    
     const one = unique.find(x => x.length === 2)!;
     const four = unique.find(x => x.length === 4)!;
     const seven = unique.find(x => x.length === 3)!;
-    decoder.set(diff(seven, one)[0], "a");
-    decoder.set(occurrences.find(x => x.counts === 8 && decoder.get(x.letter) === "?")!.letter, "c");
+    const eight = unique.find(x => x.length === 7)!;
     
-    const orBD = diff(four, one);
-    decoder.set(orBD.find(x => decoder.get(x) === "?")!, "d");
-    decoder.set(symbols.find(letter => decoder.get(letter) == "?")!, "g");
+    decoder.set(one, 1);
+    decoder.set(four, 4);
+    decoder.set(seven, 7);
+    decoder.set(eight, 8);
 
+    unique.filter(x => x.split("").length === 5).forEach(code => {
+        if (diff(code, one).length === 3) decoder.set(code, 3);
+        else if (diff(code, four).length === 3) decoder.set(code, 2);
+        else decoder.set(code, 5);
+    });
+
+    unique.filter(x => x.split("").length === 6).forEach(code => {
+        if (diff(code, one).length === 5) decoder.set(code, 6);
+        else if (diff(code, four).length === 2) decoder.set(code, 9);
+        else decoder.set(code, 0);
+    })
+    
     return decoder;
 }
 
 const part1 = (input: string): number => {
-    const lines = input.split("\r\n").map(line => line.split(" | ")[1]);
-    const digits = lines.map(line => line.split(" ")).reduce((acc, cur) => [...acc, ...cur], []);
-    const counts = digits.reduce((acc, cur) => {
-        acc[cur.length]++;
-        return acc;
-    }, Array(8).fill(0))
-    return counts[2] + counts[3] + counts[4] + counts[7];
+    const lines = input.split("\r\n").map(line => line.split(" | "));
+
+    const solved = lines.map(line => {
+        const [unique, outputs] = line.map(x => x.split(" ").map(code => code.split("").sort().join("")));
+        const decoder = getDecoder(unique);
+        return outputs.map(code => decoder.get(code)!);
+    });
+    return solved.flat().filter(x => [1,4,7,8].includes(x)).length;
 };
 
 const part2 = (input: string): number => {
     const lines = input.split("\r\n").map(line => line.split(" | "));
 
-    const solved: number[] = lines.map(line => {
-        const [unique, outputs] = line.map(x => x.split(" ").map(x => x.split("")));
-        
+    const solved = lines.map(line => {
+        const [unique, outputs] = line.map(x => x.split(" ").map(code => code.split("").sort().join("")));
         const decoder = getDecoder(unique);
-        
-        const decyphered = outputs.map(code => code.map(x => decoder.get(x)).sort().join(""));
-        const combined = decyphered.map(code => codeToDigit(code).toString()).reduce((acc, cur) => acc + cur, "");
-        return parseInt(combined);
+        const decyphered = outputs.map(code => decoder.get(code)!);
+        return +decyphered.join("");
     });
     return solved.reduce((acc, cur) => acc + cur);
 };
